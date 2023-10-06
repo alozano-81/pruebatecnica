4720fd28d42e4b164d7d3f77f2cdc125f920e2e7
Andres Lozano <yeisonandreslozano@gmail.com>
2023-10-06 17:52:13 -0500

    Modificaciones en la configuracion y prueba de funcionamiento con oracle

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,17 +3,33 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = { "com.example.demo.repository" })
-@ComponentScan({ "com.example.demo.service" })
+@ComponentScan({ "com.example.demo.controller", "com.example.demo.service" })
 @EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") //
+						.allowedOrigins("*") //
+						.allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE", "PATCH") //
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
--- src/main/java/com/example/demo/controller/test.java
+++ src/main/java/com/example/demo/controller/test.java
@@ -1,23 +1,26 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("api/prueba")
-public class test {
+import com.example.demo.service.PruebaService;
 
-    // @Autowired
-    // PruebaService pruebaService;
+@RestController
+@RequestMapping("api/tecnica")
+public class Test {
+
+    @Autowired
+    PruebaService pruebaService;
 
     @GetMapping("/entrar")
     public ResponseEntity<String> pruebaNavi() {
 
-        // pruebaService.mostrar();
-        System.out.println("llega");
+        pruebaService.mostrar();
+        System.out.println("llega1");
 
         return new ResponseEntity<>("OKi", HttpStatus.ACCEPTED);
     }
