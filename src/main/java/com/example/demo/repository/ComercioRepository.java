aa6478a2a3de6c672bd91ba9a7087feadd11b835
Andres Lozano <yeisonandreslozano@gmail.com>
2023-10-07 00:05:36 -0500

    ajustes

--- src/main/java/com/example/demo/repository/ComercioRepository.java
+++ src/main/java/com/example/demo/repository/ComercioRepository.java
@@ -1,10 +1,25 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entities.Comercios;
 
 //@Repository
 public interface ComercioRepository extends JpaRepository<Comercios, Long> {
 
+    // @Transactional
+    // @Modifying
+    @Query(value = "call print_contact(:id)", nativeQuery = true)
+    List<Object[]> verStoreProcedure(@Param("id") int id);
+
+    @Procedure("system.print_contact")
+    List<Object[]> verStoreProcedure2(@Param("id") int id);
+
+    @Query(value = " select * from comercios", nativeQuery = true)
+    List<Object[]> listar();
 }
--- src/main/java/com/example/demo/service/impl/PruebaImplService.java
+++ src/main/java/com/example/demo/service/impl/PruebaImplService.java
@@ -1,15 +1,27 @@
 package com.example.demo.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.repository.ComercioRepository;
 import com.example.demo.service.PruebaService;
 
 @Service
 public class PruebaImplService implements PruebaService {
 
+    @Autowired
+    ComercioRepository comercioRepository;
+
     @Override
     public void mostrar() {
-        System.out.println("llega");
+        List<Object[]> listaObj = new ArrayList<>();
+        listaObj = comercioRepository.verStoreProcedure2(1);
+        // List<Object[]> ver = comercioRepository.listar();
+        System.out.println("llega" + listaObj.size());
+
     }
 
 }
